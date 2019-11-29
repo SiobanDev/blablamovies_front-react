b21797bac566c6974dfc817ecb9552bc57b24aeb
Gwendoline MATHAIS <gma@cnam-lyon.peda>
2019-11-29 14:56:03 +0100

    layouts ok, functionnal components in progress

---
+++ src/components/connexionForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const connexionForm = () => {
+    return(
+        <div id="form-content" className="row">
+            <div className="col-md-8 order-md-1">
+                <h4 className="mb-3">Connexion</h4>
+                <form
+                    method="POST"
+                    id="connexion-form"
+                    className="needs-validation"
+                >
+                    <div className="mb-3">
+                        <label htmlFor="mail">Mail</label>
+
+                        <div id="invalid-email-login-feedback" className="invalid-feedback">
+                            Votre email n'est pas au bon format.
+                        </div>
+
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="mail"
+                            name="mail"
+                            placeholder="Mail"
+                            onSubmit="validateMail()"
+                            autoFocus
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="form-control"
+                            name="password"
+                            placeholder="Password"
+                        />
+                    </div>
+
+                    <div id="invalid-empty-feedback" className="invalid-feedback">
+                        Certains champs requis sont vides.
+                    </div>
+
+                    <button
+                        id="connexion-button"
+                        className="btn btn-primary btn-lg btn-block"
+                        type="submit"
+                    >
+                        Connexion
+                    </button>
+
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default connexionForm;
